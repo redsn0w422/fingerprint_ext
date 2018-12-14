43ce338456ed12345ea99af705227d0edb3d4b2a
Yasha Mostofi <yasha.mostofi@gmail.com>
2018-12-13 22:10:45 -0600

    Include current tab title in notif

--- background.js
+++ background.js
@@ -1,21 +1,21 @@
 // Facilitates sending native notifications
 
 var notify = function(title, message) {
-    var opt = {
-        type: "basic",
-        title: title,
-        message: message,
-        iconUrl: "icon.png"
-    }
-    chrome.notifications.create(opt);
+    chrome.tabs.getSelected(null, function(tab) {
+        let msg = `${tab.title}: ${message}`;
+        var opt = {
+            type: "basic",
+            title: title,
+            message: msg,
+            iconUrl: "icon.png"
+        }
+        chrome.notifications.create(opt);
+    });
     // chrome.notifications.create("fingerprint_notif", opt);
 }
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
-        notify("New message!", request.message);
+        notify("New fingerprint!", request.message);
     }
 );
