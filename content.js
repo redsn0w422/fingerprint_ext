e4e6284f58743c09b887f6a4705585c763f5d82b
Yasha Mostofi <yasha.mostofi@gmail.com>
2018-12-13 22:11:08 -0600

    Use document event for hooking

--- content.js
+++ content.js
@@ -22,19 +22,42 @@
 
 // chrome.runtime.sendMessage({message: window.location.toString()});
 
+var sendMessage = function(msg) {
+    chrome.runtime.sendMessage({message: msg});
+}
+
+document.addEventListener("hook", function(data) {
+    if (data.detail && data.detail.message) {
+        sendMessage(data.detail.message);
+    }
+});
+
 var hook = function() {
+    var newEvent = function(msg) {
+        // var event = document.createEvent('Event');
+        var event = new CustomEvent('Event', {
+            "detail": {
+                message: msg
+            }
+        });
+        event.initEvent('hook');
+        document.dispatchEvent(event);
+    }
     var wrap = function(someFunction){
         var wrappedFunction = function(){
-            var args = [...arguments].splice(0)
-            console.log(`HOOKED\tfn: ${someFunction.toString()}\nargs: ${args}`)
-            return someFunction(args)
+            var args = [...arguments].splice(0);
+            console.log(someFunction.name);
+            console.log(`HOOKED\tfn: ${someFunction.toString()}\nargs: ${args}`);
+            let msg = `${someFunction.name}`;
+            newEvent(msg);
+            return someFunction.call(window, args);
         }
         return wrappedFunction
     }
     // var test = "window.navigator.mediaDevices.enumerateDevices"
-    // var ftest = eval(test)
+    // var ftest = eval(test).bind(window)
     // ftest = wrap(ftest)
-    window.navigator.mediaDevices.enumerateDevices = wrap(window.navigator.mediaDevices.enumerateDevices)
+    window.navigator.mediaDevices.enumerateDevices = wrap(window.navigator.mediaDevices.enumerateDevices.bind(window))
 
 };
 
@@ -42,3 +65,4 @@ var script = document.createElement('script');
 script.textContent = '(' + hook.toString() + ')()';
 document.documentElement.appendChild(script);
 // why are extensions "isloated" if you can just do this...
+// https://groups.google.com/a/chromium.org/forum/#!topic/chromium-extensions/LCZCkraIBdg
